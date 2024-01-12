fb2e3d3b180ab4c2a62f6633fba3d6010cf378b2
Frederico Tavares <fredericotp@hotmail.com>
2024-01-12 10:47:57 -0300

    Feat: Criando o service e injetando no componente

--- 5-services-angular/src/app/components/card/card.component.ts
+++ 5-services-angular/src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -10,9 +11,10 @@ export class CardComponent implements OnInit {
   attributesTypes: string[] = []
   photoCover: string = ""
 
-  constructor() { }
+  constructor(private service: PokemonService) { }
 
   ngOnInit(): void {
+    this.service.getPokemon("pikachu")
   }
 
 }
